fbe3801343b654732edd230c06667c5952a72390
huangsen <huangsenmx2@gmail.com>
2016-12-26 13:42:27 +0800

    日志系统转移到logback

--- web/src/main/java/com/controller/LoginController.java
+++ web/src/main/java/com/controller/LoginController.java
@@ -2,8 +2,8 @@ package com.controller;
 
 import com.ssm.bean.User;
 import com.ssm.service.User.IUserService;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/user")
 public class LoginController {
 
-    private static final Logger logger = LogManager.getLogger(LoginController.class.getName());
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class.getName());
 
     @Autowired
     IUserService userService;
@@ -26,14 +26,14 @@ public class LoginController {
     @ResponseBody
     public String login(String userName, String passWord) {
         if (StringUtils.isEmpty(userName)) {
-            logger.error("username can't be null!");
+            logger.debug("username can't be null!");
             return "error";
         }
         if (StringUtils.isEmpty(passWord)) {
-            logger.error("password can't be null!");
+            logger.debug("password can't be null!");
             return "error";
         }
-        logger.debug(userName + "is login");
+        logger.info(userName + "is login");
         User user = userService.login(userName, passWord);
         return user.toString();
     }
